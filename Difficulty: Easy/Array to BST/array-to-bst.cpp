42801743c1a3e8ffdd0c2605319de7b30d448761
jugadu_enginer <138850887+haryanaaalehr05@users.noreply.github.com>
2024-07-25 23:19:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Array to BST/array-to-bst.cpp
+++ Difficulty: Easy/Array to BST/array-to-bst.cpp
@@ -79,20 +79,20 @@ struct Node {
 
 class Solution {
   public:
-    Node* loveneesh(vector<int>& nums,int left,int right){
+    Node* vanshika(vector<int>& nums,int left,int right){
         //base case
         if(left>right){
             return NULL;
         }
         int mid= left + (right-left)/2;
         Node* node =new Node(nums[mid]);
-        node->left=loveneesh(nums,left,mid-1);
-        node->right=loveneesh(nums,mid+1,right);
+        node->left=vanshika(nums,left,mid-1);
+        node->right=vanshika(nums,mid+1,right);
     }
     
     Node* sortedArrayToBST(vector<int>& nums) {
         // Code here
-        Node* root=loveneesh(nums,0,nums.size()-1);
+        Node* root=vanshika(nums,0,nums.size()-1);
         return root;
     }
 };
