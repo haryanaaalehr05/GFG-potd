def7c05f50c5f361b6385961d6f054d350478796
jugadu_enginer <138850887+haryanaaalehr05@users.noreply.github.com>
2024-08-22 20:25:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Alien Dictionary/alien-dictionary.cpp
@@ -0,0 +1,112 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    string findOrder(string dict[], int n, int k) {
+        vector<int> adj[k];
+        // Step 1: Build the graph
+        for (int i = 0; i < n - 1; i++) {
+            string word1 = dict[i];
+            string word2 = dict[i + 1];
+            for (int j = 0; j < min(word1.size(), word2.size()); j++) {
+                if (word1[j] != word2[j]) {
+                    adj[word1[j] - 'a'].push_back(word2[j] - 'a');
+                    break;
+                }
+            }
+        }
+
+        // Step 2: Topological Sort (Kahn's Algorithm)
+        vector<int> inDegree(k, 0);
+        for (int i = 0; i < k; i++) {
+            for (auto it : adj[i]) {
+                inDegree[it]++;
+            }
+        }
+
+        queue<int> q;
+        for (int i = 0; i < k; i++) {
+            if (inDegree[i] == 0) {
+                q.push(i);
+            }
+        }
+
+        string order;
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+            order += (char)(node + 'a');
+            for (auto it : adj[node]) {
+                inDegree[it]--;
+                if (inDegree[it] == 0) {
+                    q.push(it);
+                }
+            }
+        }
+
+        // If order contains all k characters, it's a valid order
+        return order;
+    }
+};
+
+//{ Driver Code Starts.
+string order;
+
+bool f(string a, string b) {
+    int p1 = 0;
+    int p2 = 0;
+    for (int i = 0; i < min(a.size(), b.size()) and p1 == p2; i++) {
+        p1 = order.find(a[i]);
+        p2 = order.find(b[i]);
+        //	cout<<p1<<" "<<p2<<endl;
+    }
+
+    if (p1 == p2 and a.size() != b.size())
+        return a.size() < b.size();
+
+    return p1 < p2;
+}
+
+// Driver program to test above functions
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N, K;
+        cin >> N >> K;
+        string dict[N];
+        for (int i = 0; i < N; i++)
+            cin >> dict[i];
+
+        Solution obj;
+        string ans = obj.findOrder(dict, N, K);
+        order = "";
+        for (int i = 0; i < ans.size(); i++)
+            order += ans[i];
+
+        string temp[N];
+        std::copy(dict, dict + N, temp);
+        sort(temp, temp + N, f);
+
+        bool f = true;
+        for (int i = 0; i < N; i++)
+            if (dict[i] != temp[i])
+                f = false;
+
+        if (f)
+            cout << 1;
+        else
+            cout << 0;
+        cout << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
